e9e9e084cad35c59337af6e76a3ae7090609a25d
Sjors Provoost <sjors@sprovoost.nl>
2017-02-03 18:48:22 +0100

    Use Simplecov to generate local coverage file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
+require 'simplecov'
 require 'coveralls'
 
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-Coveralls.wear!
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+end
 
 require "one_hour_api"
